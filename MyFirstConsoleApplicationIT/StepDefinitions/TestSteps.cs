9339609e93c22e58fe50d86295f2fbbba3bf7c39
Anniek van Dijk <anniek@animundo.nl>
2016-11-26 15:19:15 +0100

    Added UnitTest project to Solution

--- MyFirstConsoleApplicationIT/StepDefinitions/TestSteps.cs
+++ MyFirstConsoleApplicationIT/StepDefinitions/TestSteps.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyFirstConsoleApplication.Animal;
 using MyFirstConsoleApplicationIT.Actions;
 using TechTalk.SpecFlow;
---
+++ MyFirstConsoleApplicationTest/UnitTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstConsoleApplication.Animal;
+
+namespace MyFirstConsoleApplicationTest
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void AddDog()
+        {
+            var doggie = new Dog("Doggy", 4, 0.4f, DogBreed.Boxer);
+            Assert.AreEqual("Doggy", doggie.name);
+        }
+
+        [TestMethod]
+        public void DogHappyness()
+        {
+            var doggie = new Dog("Doggy", 4, 0.4f, DogBreed.Boxer);
+            doggie.Bark();
+            Assert.AreEqual(0.5f, doggie.happiness);
+        }
+
+        [TestMethod]
+        public void NumberOfPets()
+        {
+            var doggie = new Dog("Doggy", 4, 0.4f, DogBreed.Boxer);
+            var spotty = new Dog("Spotty", 2, 0.1f, DogBreed.Briard);
+            Assert.AreEqual(2, Animal.Count);
+        }
+
+    }
+}
